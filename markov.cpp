4978e655a8bbd8c42cc088c874f10e192f2bd2c9
TheVC-Dev <VicBCJ@proton.me>
2026-02-25 13:51:22 -0800

    Implemented joinwords

--- markov.cpp
+++ markov.cpp
@@ -12,3 +12,12 @@ void getInp(string& fileInp, int& orderInp, int& wordnumInp){
     cout << "Enter number of words to generate: ";
     cin >> wordnumInp;
 }
+
+string joinWords(const string words[], int startIndex, int count){
+    string result;
+    for(int i = startIndex; i < startIndex + count; i++){
+        result += words[i];
+    }
+    
+    return result;
+}
