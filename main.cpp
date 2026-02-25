0a22dc01784a91b1f9b1b1d59941a97513cb42ac
TheVC-Dev <VicBCJ@proton.me>
2026-02-25 13:37:09 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include  <iostream>
+#include "markov.h"
+#include "markov.cpp"
+#include <cstdlib>
+#include <ctime>
+
+using namespace std; 
+
+int main(){
+    srand(time(0));
+
+    return 0;
+}
