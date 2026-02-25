9291e200de2b991d6ef604a373225b434bffa7c3
TheVC-Dev <VicBCJ@proton.me>
2026-02-25 14:13:22 -0800

    removed the debugging lines

--- markov.cpp
+++ markov.cpp
@@ -40,11 +40,8 @@ int buildMarkovChain(const string words[], int numWords, int order, string prefi
     int count = 0;
     for(int i= 0; i <=(numWords - order - 1); i++){
         prefixes[count] += joinWords(words, i, order);
-        cout << "Prefix: " << joinWords(words, i, order) << endl;
-
         suffixes[count] += words[i + order];
-        cout << "Suffix: " << words[i + order] << endl;
-
+        
         count++;
     }
     return count;
@@ -52,42 +49,25 @@ int buildMarkovChain(const string words[], int numWords, int order, string prefi
 
 string getRandomSuffix(const string prefixes[], const string suffixes[], int chainSize, string currentPrefix){
     int matchcount = 0;
-    cout << "cainsize: " << chainSize << endl;
     string matches[chainSize];
-
+    
     for(int i = 0; i < (chainSize); i++){
-
-        cout << "suffixes: " << suffixes[i] << endl;
         if (currentPrefix == prefixes[i]){
-            cout << currentPrefix << "==" << prefixes[i] << "   adding:  " << suffixes[i] << endl;
             matches[matchcount] += suffixes[i];
             matchcount++;
         }
     }
-
+    
     if(matchcount){
         int pick = rand() % matchcount;
-        
-        cout << "matches: ";
-        for (int i = 0; i < matchcount; i++){
-            cout << matches[i] << ", ";
-        }
-        cout << endl;
-
-        cout << "Randum index: " << pick << endl;
-        cout << "choosing:... "  << matches[pick] << endl;
         string random_matching_index = matches[pick];
 
         return random_matching_index;
-    }else{return "no match";}
+    }else { return "no match"; }
 }
 
 string getRandomPrefix(const string prefixes[], int chainSize){
     int randomIndex = rand() % chainSize;
-
-    cout << "random prefix index: " << randomIndex << endl;
-    cout << "random prefix: " << prefixes[randomIndex] << endl;
-
     return prefixes[randomIndex];
 }
 
@@ -100,7 +80,6 @@ string generateText(const string prefixes[], const string suffixes[], int chainS
         if(i == 0){
             currentPrefix = getRandomPrefix(prefixes, chainSize);
             result[0] = currentPrefix;
-            cout << "first prefix: " << result[0] << endl;
         }else{
             updateSuffix = getRandomSuffix(prefixes, suffixes, chainSize, currentPrefix);
             if(updateSuffix != "no match"){
@@ -110,9 +89,7 @@ string generateText(const string prefixes[], const string suffixes[], int chainS
         }
 
     }
-
-
-    cout << "first result: " << result[0] << endl; 
+    
     for (int i = 0; i < numWords; i++){
         cout << "result: " << result[i] << endl;
     }
