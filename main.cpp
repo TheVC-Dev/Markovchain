1c0adb6e1b95460025227f736db85b77c40eb1e5
TheVC-Dev <VicBCJ@proton.me>
2026-02-25 13:55:47 -0800

    Implemented readWordsfromFile

--- main.cpp
+++ main.cpp
@@ -10,8 +10,15 @@ int main(){
     srand(time(0));
     string fileInp; 
     int orderInp, wordnumInp;
+    string prefixes[10000], suffixes[10000], words[10000], result[10000];
 
     getInp(fileInp, orderInp, wordnumInp);
+
+    int count{readWordsfromFile(fileInp, words, 50)};
+    cout << "Read " << count << " words" << endl;
+    for (int i = 0; i < 10 && i < count; i++){
+        cout << words[i] << endl;
+    }
 
     return 0;
 }
